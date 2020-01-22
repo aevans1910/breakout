68fbb8d570ef28cf0c07a163e0cb0297c4a538be
Ariane Evans <ariane.evans@student.makeschool.com>
2020-01-21 19:23:39 -0800

    [add] bricks

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ let paddleX = (canvas.width-paddleWidth)/2;
 let rightPressed = false;
 let leftPressed = false;
 
+// Brick variables
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+let bricks = [];
+for (let c=0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r=0; r < brickRowCount; r++) {
+        bricks[c][r] = {x:0, y:0};
+    }
+}
+
+// Event handlers
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -53,8 +71,25 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c=0; c < brickColumnCount; c++) {
+        for (let r=0; r < brickRowCount ; r++) {
+            let brickX = (c* (brickWidth + brickPadding)) + brickOffsetLeft;
+            let brickY = (r* (brickHeight + brickPadding)) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
 
